730838afc130925765d2064970f6d02ca412e5a8
spevnev <spevnev16@gmail.com>
2024-04-01 09:26:44 +0300

    Improve error handling

--- src/error.h
+++ src/error.h
@@ -1,15 +1,24 @@
 #ifndef ERROR_H
 #define ERROR_H
 
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define ERROR(...)                    \
-    do {                              \
-        fprintf(stderr, "ERROR: ");   \
-        fprintf(stderr, __VA_ARGS__); \
-        fflush(stderr);               \
-        exit(1);                      \
+#ifdef DEBUG
+#    define _ERROR() fprintf(stderr, "errno = \"%s\".\n", strerror(errno));
+#else
+#    define _ERROR()
+#endif
+
+#define ERROR(...)                                             \
+    do {                                                       \
+        fprintf(stderr, "ERROR(%s:%d): ", __FILE__, __LINE__); \
+        fprintf(stderr, __VA_ARGS__);                          \
+        _ERROR();                                              \
+        fflush(stderr);                                        \
+        exit(1);                                               \
     } while (0);
 
 #endif  // ERROR_H
--- src/git.c
+++ src/git.c
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include "error.h"
@@ -15,28 +16,29 @@ char *git_exec(int *status, char *const *args) {
     assert(status != NULL);
 
     int pipe_fds[2];
-    if (pipe(pipe_fds) == -1) goto error;
+    if (pipe(pipe_fds) == -1) ERROR("Couldn't open pipe.\n");
 
     pid_t pid = fork();
-    if (pid == -1) goto error;
+    if (pid == -1) ERROR("Couldn't fork process.\n");
 
     if (pid == 0) {
         int write_fd = pipe_fds[1];
-        if (close(pipe_fds[0]) == -1) abort();
+        if (close(pipe_fds[0]) == -1) exit(1);
 
-        if (dup2(write_fd, STDOUT_FILENO) == -1) abort();
-        if (dup2(write_fd, STDERR_FILENO) == -1) abort();
+        if (dup2(write_fd, STDOUT_FILENO) == -1) exit(1);
+        if (dup2(write_fd, STDERR_FILENO) == -1) exit(1);
+
         if (execvp("git", args) == -1 && errno == ENOENT) exit(NO_GIT_BINARY);
     }
 
     int read_fd = pipe_fds[0];
-    if (close(pipe_fds[1]) == -1) goto error;
+    if (close(pipe_fds[1]) == -1) ERROR("Couldn't close pipe.\n");
 
     size_t buffer_capacity = INITIAL_BUFFER_SIZE;
     size_t buffer_size = buffer_capacity;
     size_t buffer_offset = 0;
     char *buffer = (char *) malloc(buffer_capacity + 1);
-    if (buffer == NULL) ERROR("Couldn't run git command. Process is out of memory.\n");
+    if (buffer == NULL) ERROR("Process is out of memory.\n");
 
     int bytes;
     while ((bytes = read(read_fd, buffer + buffer_offset, buffer_size)) > 0) {
@@ -47,20 +49,18 @@ char *git_exec(int *status, char *const *args) {
             buffer_size = buffer_capacity;
             buffer_capacity *= 2;
             buffer = (char *) realloc(buffer, buffer_capacity + 1);
-            if (buffer == NULL) ERROR("Couldn't run git command. Process is out of memory.\n");
+            if (buffer == NULL) ERROR("Process is out of memory.\n");
         }
     }
-    if (bytes == -1) goto error;
+    if (bytes == -1) ERROR("Couldn't read from child's pipe.\n");
     buffer[buffer_offset] = '\0';
 
-    if (waitpid(pid, status, 0) == -1) goto error;
+    if (waitpid(pid, status, 0) == -1) ERROR("Couldn't wait for child process.\n");
     if (WEXITSTATUS(*status) == NO_GIT_BINARY) ERROR("Couldn't find git binary. Make sure it is in PATH.\n");
 
-    if (close(read_fd) == -1) goto error;
+    if (close(read_fd) == -1) ERROR("Couldn't close pipe.\n");
 
     return buffer;
-error:
-    ERROR("Couldn't run git command due to an unexpected error.\n");
 }
 
 int is_git_initialized(void) {
