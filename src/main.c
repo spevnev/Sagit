bf5bd6eff9bb613f17b0907a1be6487c84b195b9
spevnev <spevnev16@gmail.com>
2024-04-01 21:42:15 +0300

    ncurses

--- src/main.c
+++ src/main.c
@@ -1,7 +1,72 @@
+#include <assert.h>
+#include <locale.h>
+#include <ncurses.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "error.h"
 #include "git.h"
+
+void render(GitState git) {
+    if (git.untracked.files.length > 0) {
+        printw("Unstaged files:\n");
+
+        for (size_t i = 0; i < git.untracked.files.length; i++) {
+            printw("created %s\n", git.untracked.files.data[i]);
+        }
+
+        printw("\n");
+    }
+
+    if (git.unstaged.files.length > 0) {
+        printw("Unstaged changes:\n");
+
+        for (size_t i = 0; i < git.unstaged.files.length; i++) {
+            File file = git.unstaged.files.data[i];
+
+            if (strcmp(file.dest, "/dev/null") == 0) {
+                printw("deleted  %s\n", file.src + 2);
+            } else if (strcmp(file.src + 2, file.dest + 2) == 0) {
+                printw("renamed  %s -> %s\n", file.src + 2, file.dest + 2);
+            } else {
+                printw("modified %s\n", file.src + 2);
+            }
+
+            for (size_t i = 0; i < file.hunks.length; i++) {
+                str_vec hunk = file.hunks.data[i];
+                for (size_t j = 0; j < hunk.length; j++) {
+                    printw("%s\n", hunk.data[j]);
+                }
+            }
+        }
+
+        printw("\n");
+    }
+
+    if (git.staged.files.length > 0) {
+        printw("Staged changes:\n");
+
+        for (size_t i = 0; i < git.staged.files.length; i++) {
+            File file = git.staged.files.data[i];
+
+            if (strcmp(file.dest, "/dev/null") == 0) {
+                printw("deleted  %s\n", file.src + 2);
+            } else if (strcmp(file.src + 2, file.dest + 2) == 0) {
+                printw("renamed  %s -> %s\n", file.src + 2, file.dest + 2);
+            } else {
+                printw("modified %s\n", file.src + 2);
+            }
+
+            for (size_t i = 0; i < file.hunks.length; i++) {
+                str_vec hunk = file.hunks.data[i];
+                for (size_t j = 0; j < hunk.length; j++) {
+                    printw("%s\n", hunk.data[j]);
+                }
+            }
+        }
+
+        printw("\n");
+    }
+}
 
 int main(int argc, char **argv) {
     if (argc > 1) {
@@ -14,65 +79,38 @@ int main(int argc, char **argv) {
     GitState git = {0};
     if (get_git_state(&git)) ERROR("Unable to get git state.\n");
 
-    if (git.untracked.files.length > 0) {
-        printf("Unstaged files:\n");
+    setlocale(LC_ALL, "");
+    initscr();
+    cbreak();
+    noecho();
+    start_color();
 
-        for (size_t i = 0; i < git.untracked.files.length; i++) {
-            printf("created %s\n", git.untracked.files.data[i]);
+    render(git);  // TODO: pointer
+    move(0, 0);
+
+    char running = 1;
+    while (running) {
+        switch (getch()) {
+            case 'q':
+                running = false;
+                break;
+            case 'h':
+                assert(false);  // TODO:
+                break;
+            case 'j':
+                assert(false);  // TODO:
+                break;
+            case 'k':
+                assert(false);  // TODO:
+                break;
+            default:
+                break;
         }
 
-        printf("\n");
+        refresh();
     }
 
-    if (git.unstaged.files.length > 0) {
-        printf("Unstaged changes:\n");
-
-        for (size_t i = 0; i < git.unstaged.files.length; i++) {
-            File file = git.unstaged.files.data[i];
-
-            if (strcmp(file.dest, "/dev/null") == 0) {
-                printf("deleted  %s\n", file.src + 2);
-            } else if (strcmp(file.src + 2, file.dest + 2) == 0) {
-                printf("renamed  %s -> %s\n", file.src + 2, file.dest + 2);
-            } else {
-                printf("modified %s\n", file.src + 2);
-            }
-
-            for (size_t i = 0; i < file.hunks.length; i++) {
-                str_vec hunk = file.hunks.data[i];
-                for (size_t j = 0; j < hunk.length; j++) {
-                    printf("%s\n", hunk.data[j]);
-                }
-            }
-        }
-
-        printf("\n");
-    }
-
-    if (git.staged.files.length > 0) {
-        printf("Staged changes:\n");
-
-        for (size_t i = 0; i < git.staged.files.length; i++) {
-            File file = git.staged.files.data[i];
-
-            if (strcmp(file.dest, "/dev/null") == 0) {
-                printf("deleted  %s\n", file.src + 2);
-            } else if (strcmp(file.src + 2, file.dest + 2) == 0) {
-                printf("renamed  %s -> %s\n", file.src + 2, file.dest + 2);
-            } else {
-                printf("modified %s\n", file.src + 2);
-            }
-
-            for (size_t i = 0; i < file.hunks.length; i++) {
-                str_vec hunk = file.hunks.data[i];
-                for (size_t j = 0; j < hunk.length; j++) {
-                    printf("%s\n", hunk.data[j]);
-                }
-            }
-        }
-
-        printf("\n");
-    }
+    endwin();
 
     free(git.untracked.raw);
     VECTOR_FREE(&git.untracked.files);
