f43b89d2692ca77f13ae4f5f537aa6c273b1fe8f
spevnev <spevnev16@gmail.com>
2024-04-02 15:03:35 +0300

    Add scrolling and actions

--- src/main.c
+++ src/main.c
@@ -8,67 +8,95 @@
 #include "error.h"
 #include "git.h"
 
+typedef int action_t(void *, int);
+DEFINE_VECTOR_TYPE(action_vec, action_t *);
+
 static char running = 1;
 static GitState git = {0};
+static str_vec lines = {0};
+static action_vec actions = {0};
+static void_vec action_args = {0};
+
+#define ADD_LINE(action, arg, ...)                              \
+    do {                                                        \
+        VECTOR_PUSH(&actions, action);                          \
+        VECTOR_PUSH(&action_args, arg);                         \
+        size_t size = snprintf(NULL, 0, __VA_ARGS__) + 1;       \
+        char *buffer = (char *) malloc(size);                   \
+        if (buffer == NULL) ERROR("Process is out of memory."); \
+        snprintf(buffer, size, __VA_ARGS__);                    \
+        VECTOR_PUSH(&lines, buffer);                            \
+    } while (0)
+
 static void render() {
+    for (size_t i = 0; i < lines.length; i++) free(lines.data[i]);
+    lines.length = 0;
+    actions.length = 0;
+
+    // TODO: add colors
+
     if (git.untracked.files.length > 0) {
-        printw("Unstaged files:\n");
+        ADD_LINE(NULL, NULL, "Unstaged files:\n");
 
         for (size_t i = 0; i < git.untracked.files.length; i++) {
-            printw("created %s\n", git.untracked.files.data[i]);
+            ADD_LINE(NULL, NULL, "created %s\n", git.untracked.files.data[i]);
         }
 
-        printw("\n");
+        ADD_LINE(NULL, NULL, "\n");
     }
 
     if (git.unstaged.files.length > 0) {
-        printw("Unstaged changes:\n");
+        ADD_LINE(NULL, NULL, "Unstaged changes:\n");
 
         for (size_t i = 0; i < git.unstaged.files.length; i++) {
             File file = git.unstaged.files.data[i];
 
             if (strcmp(file.dest, "/dev/null") == 0) {
-                printw("deleted  %s\n", file.src + 2);
+                ADD_LINE(NULL, NULL, "deleted  %s\n", file.src + 2);
             } else if (strcmp(file.src + 2, file.dest + 2) == 0) {
-                printw("renamed  %s -> %s\n", file.src + 2, file.dest + 2);
+                ADD_LINE(NULL, NULL, "renamed  %s -> %s\n", file.src + 2, file.dest + 2);
             } else {
-                printw("modified %s\n", file.src + 2);
+                ADD_LINE(NULL, NULL, "modified %s\n", file.src + 2);
             }
 
             for (size_t i = 0; i < file.hunks.length; i++) {
-                str_vec hunk = file.hunks.data[i];
-                for (size_t j = 0; j < hunk.length; j++) {
-                    printw("%s\n", hunk.data[j]);
+                str_vec *hunk = &file.hunks.data[i];
+
+                ADD_LINE(NULL, NULL, "%s\n", hunk->data[0]);
+                for (size_t j = 1; j < hunk->length; j++) {
+                    ADD_LINE(NULL, NULL, "%s\n", hunk->data[j]);
                 }
             }
         }
 
-        printw("\n");
+        ADD_LINE(NULL, NULL, "\n");
     }
 
     if (git.staged.files.length > 0) {
-        printw("Staged changes:\n");
+        ADD_LINE(NULL, NULL, "Staged changes:\n");
 
         for (size_t i = 0; i < git.staged.files.length; i++) {
             File file = git.staged.files.data[i];
 
             if (strcmp(file.dest, "/dev/null") == 0) {
-                printw("deleted  %s\n", file.src + 2);
+                ADD_LINE(NULL, NULL, "deleted  %s\n", file.src + 2);
             } else if (strcmp(file.src + 2, file.dest + 2) == 0) {
-                printw("renamed  %s -> %s\n", file.src + 2, file.dest + 2);
+                ADD_LINE(NULL, NULL, "renamed  %s -> %s\n", file.src + 2, file.dest + 2);
             } else {
-                printw("modified %s\n", file.src + 2);
+                ADD_LINE(NULL, NULL, "modified %s\n", file.src + 2);
             }
 
             for (size_t i = 0; i < file.hunks.length; i++) {
-                str_vec hunk = file.hunks.data[i];
-                for (size_t j = 0; j < hunk.length; j++) {
-                    printw("%s\n", hunk.data[j]);
+                str_vec *hunk = &file.hunks.data[i];
+
+                ADD_LINE(NULL, NULL, "%s\n", hunk->data[0]);
+                for (size_t j = 1; j < hunk->length; j++) {
+                    ADD_LINE(NULL, NULL, "%s\n", hunk->data[j]);
                 }
             }
         }
 
-        printw("\n");
+        ADD_LINE(NULL, NULL, "\n");
     }
 }
 
@@ -83,6 +111,10 @@ static void cleanup() {
     free_files(&git.staged.files);
 
     for (size_t i = 0; i < lines.length; i++) free(lines.data[i]);
+
+    VECTOR_FREE(&lines);
+    VECTOR_FREE(&actions);
+    VECTOR_FREE(&action_args);
 }
 
 static void stop_running(int signal) {
@@ -112,9 +144,21 @@ int main(int argc, char **argv) {
     action.sa_handler = stop_running;
     sigaction(SIGINT, &action, NULL);
 
+    size_t scroll = 0;
+    size_t cursor = 0;
 
     while (running) {
-        switch (getch()) {
+        size_t height = getmaxy(stdscr);
+
+        clear();
+        for (size_t i = 0; i < height; i++) {
+            if (scroll + i < lines.length) printw("%s", lines.data[scroll + i]);
+        }
+        move(cursor, 0);
+        refresh();
+
+        int ch = getch();
+        switch (ch) {
             case 'q':
                 running = false;
                 break;
@@ -122,13 +166,21 @@ int main(int argc, char **argv) {
                 assert(false);  // TODO:
                 break;
             case 'j':
-                assert(false);  // TODO:
+                if (cursor < height - 1) cursor++;
+                else if (scroll + height - 1 < lines.length) scroll++;
                 break;
             case 'k':
-                assert(false);  // TODO:
+                if (cursor > 0) cursor--;
+                else if (scroll > 0) scroll--;
                 break;
-            default:
-                break;
+            default: {
+                int i = scroll + cursor;
+                action_t *action = actions.data[i];
+                if (action != NULL) {
+                    int rerender = action(action_args.data[i], ch);
+                    if (rerender) render();
+                }
+            } break;
         }
     }
 
