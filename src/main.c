8ae617b07103cc5029d77b328c4fe3a1e78718b9
spevnev <spevnev16@gmail.com>
2024-04-02 18:53:00 +0300

    Make ADD_LINE and APPEND_LINE add \n

--- src/main.c
+++ src/main.c
@@ -25,21 +25,28 @@ static void_vec action_args = {0};
         VECTOR_PUSH(&actions, action);                        \
         VECTOR_PUSH(&action_args, arg);                       \
         size_t size = snprintf(NULL, 0, __VA_ARGS__) + 1;     \
-        char *line = (char *) malloc(size);                   \
+        char *line = (char *) malloc(size + 1);               \
         if (line == NULL) ERROR("Process is out of memory."); \
         snprintf(line, size, __VA_ARGS__);                    \
+        line[size - 1] = '\n';                                \
+        line[size] = '\0';                                    \
         VECTOR_PUSH(&lines, line);                            \
     } while (0)
 
-#define APPEND_LINE(...)                                                                       \
-    do {                                                                                       \
-        assert(lines.length > 0);                                                              \
-        char *old_line = lines.data[lines.length - 1];                                         \
-        size_t old_size = strlen(old_line);                                                    \
-        size_t additional_size = snprintf(NULL, 0, __VA_ARGS__) + 1;                           \
-        lines.data[lines.length - 1] = (char *) realloc(old_line, old_size + additional_size); \
-        if (lines.data[lines.length - 1] == NULL) ERROR("Process is out of memory.");          \
-        snprintf(lines.data[lines.length - 1] + old_size - 1, additional_size, __VA_ARGS__);   \
+#define EMPTY_LINE() ADD_LINE(NULL, NULL, "")
+
+#define APPEND_LINE(...)                                           \
+    do {                                                           \
+        assert(lines.length > 0);                                  \
+        char **line_ptr = &lines.data[lines.length - 1];           \
+        size_t old_size = strlen(*line_ptr) - 1;                   \
+        size_t add_size = snprintf(NULL, 0, __VA_ARGS__) + 1;      \
+        size_t new_size = old_size + add_size;                     \
+        *line_ptr = (char *) realloc(*line_ptr, new_size + 1);     \
+        if (*line_ptr == NULL) ERROR("Process is out of memory."); \
+        snprintf((*line_ptr) + old_size, add_size, __VA_ARGS__);   \
+        (*line_ptr)[new_size - 1] = '\n';                          \
+        (*line_ptr)[new_size] = '\0';                              \
     } while (0)
 
 static int hunk_action(void *_hunk, int ch) {
@@ -79,24 +86,24 @@ static void render_files(FileVec *files) {
     for (size_t i = 0; i < files->length; i++) {
         File *file = &files->data[i];
 
-        ADD_LINE(&file_action, file, "%s\n", FOLD_CHAR(file->is_folded));
+        ADD_LINE(&file_action, file, "%s", FOLD_CHAR(file->is_folded));
         if (strcmp(file->dest, "/dev/null") == 0) {
-            APPEND_LINE("deleted  %s\n", file->src + 2);
+            APPEND_LINE("deleted  %s", file->src + 2);
         } else if (strcmp(file->src + 2, file->dest + 2) != 0) {
-            APPEND_LINE("renamed  %s -> %s\n", file->src + 2, file->dest + 2);
+            APPEND_LINE("renamed  %s -> %s", file->src + 2, file->dest + 2);
         } else {
-            APPEND_LINE("modified %s\n", file->src + 2);
+            APPEND_LINE("modified %s", file->src + 2);
         }
         if (file->is_folded) continue;
 
         for (size_t i = 0; i < file->hunks.length; i++) {
             Hunk *hunk = &file->hunks.data[i];
 
-            ADD_LINE(&hunk_action, hunk, "%s%s\n", FOLD_CHAR(hunk->is_folded), hunk->header);
+            ADD_LINE(&hunk_action, hunk, "%s%s", FOLD_CHAR(hunk->is_folded), hunk->header);
             if (hunk->is_folded) continue;
 
             for (size_t j = 0; j < hunk->lines.length; j++) {
-                ADD_LINE(NULL, NULL, "%s\n", hunk->lines.data[j]);
+                ADD_LINE(NULL, NULL, "%s", hunk->lines.data[j]);
             }
         }
     }
@@ -111,25 +118,25 @@ static void render(void) {
     // TODO: add colors, italics/bold
 
     if (state.untracked.files.length > 0) {
-        ADD_LINE(&section_action, &state.untracked, "%sUntracked files:\n", FOLD_CHAR(state.untracked.is_folded));
+        ADD_LINE(&section_action, &state.untracked, "%sUntracked files:", FOLD_CHAR(state.untracked.is_folded));
         if (!state.untracked.is_folded) {
             for (size_t i = 0; i < state.untracked.files.length; i++) {
-                ADD_LINE(NULL, NULL, "created %s\n", state.untracked.files.data[i]);
+                ADD_LINE(NULL, NULL, "created %s", state.untracked.files.data[i]);
             }
         }
-        ADD_LINE(NULL, NULL, "\n");
+        EMPTY_LINE();
     }
 
     if (state.unstaged.files.length > 0) {
-        ADD_LINE(&section_action, &state.unstaged, "%sUnstaged files:\n", FOLD_CHAR(state.unstaged.is_folded));
+        ADD_LINE(&section_action, &state.unstaged, "%sUnstaged changes:", FOLD_CHAR(state.unstaged.is_folded));
         if (!state.unstaged.is_folded) render_files(&state.unstaged.files);
-        ADD_LINE(NULL, NULL, "\n");
+        EMPTY_LINE();
     }
 
     if (state.staged.files.length > 0) {
-        ADD_LINE(&section_action, &state.staged, "%sStaged files:\n", FOLD_CHAR(state.staged.is_folded));
+        ADD_LINE(&section_action, &state.staged, "%sStaged changes:", FOLD_CHAR(state.staged.is_folded));
         if (!state.staged.is_folded) render_files(&state.staged.files);
-        ADD_LINE(NULL, NULL, "\n");
+        EMPTY_LINE();
     }
 }
 
@@ -200,7 +207,7 @@ int main(int argc, char **argv) {
                 break;
             case 'j':
                 if (cursor < height - 1) cursor++;
-                else if (scroll + height - 1 < lines.length) scroll++;
+                else if (scroll + height < lines.length) scroll++;
                 break;
             case 'k':
                 if (cursor > 0) cursor--;
