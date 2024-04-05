7d8301a044053b02338d385152604ad4129dae0c
spevnev <spevnev16@gmail.com>
2024-04-05 19:10:44 +0300

    Add other actions

--- src/action.c
+++ src/action.c
@@ -14,38 +14,91 @@ int section_action(void *_section, int ch) {
     return 0;
 }
 
-int file_action(void *_file, int ch) {
+int untracked_file_action(void *_file_path, int ch) {
+    char *file_path = (char *) _file_path;
+
+    if (ch == 's') {
+        git_stage_file(file_path);
+        return AC_RERENDER | AC_UPDATE_STATE;
+    }
+
+    return 0;
+}
+
+int unstaged_file_action(void *_file, int ch) {
+    File *file = (File *) _file;
+
+    if (ch == ' ') {
+        file->is_folded ^= 1;
+        return AC_RERENDER;
+    } else if (ch == 's') {
+        git_stage_file(file->src + 2);
+        return AC_RERENDER | AC_UPDATE_STATE;
+    }
+
+    return 0;
+}
+
+int staged_file_action(void *_file, int ch) {
     File *file = (File *) _file;
 
     if (ch == ' ') {
         file->is_folded ^= 1;
         return AC_RERENDER;
     } else if (ch == 'u') {
-        // TODO: check that it is staged
         git_unstage_file(file->dest + 2);
-        return AC_RERENDER | AC_UPDATE_STATE;
-    } else if (ch == 's') {
-        // TODO: check that it is unstaged/tracked
-        git_stage_file(file);
         return AC_RERENDER | AC_UPDATE_STATE;
     }
 
     return 0;
 }
 
-int hunk_action(void *_hunk, int ch) {
+int unstaged_hunk_action(void *_hunk, int ch) {
     Hunk *hunk = (Hunk *) _hunk;
 
     if (ch == ' ') {
         hunk->is_folded ^= 1;
         return AC_RERENDER;
+    } else if (ch == 's') {
+        // git_unstage_hunk(file, hunk);
+        return AC_RERENDER | AC_UPDATE_STATE;
     }
 
     return 0;
 }
 
-int line_action(void *_line, int ch) {
+int staged_hunk_action(void *_hunk, int ch) {
+    Hunk *hunk = (Hunk *) _hunk;
+
+    if (ch == ' ') {
+        hunk->is_folded ^= 1;
+        return AC_RERENDER;
+    } else if (ch == 'u') {
+        // git_unstage_hunk(file, hunk);
+        return AC_RERENDER | AC_UPDATE_STATE;
+    }
+
+    return 0;
+}
+
+int unstaged_line_action(void *_line, int ch) {
     char *line = (char *) _line;
+
+    if (ch == 's') {
+        // git_stage_line(file, line);
+        return AC_RERENDER | AC_UPDATE_STATE;
+    }
+
+    return 0;
+}
+
+int staged_line_action(void *_line, int ch) {
+    char *line = (char *) _line;
+
+    if (ch == 'u') {
+        // git_unstage_line(file, line);
+        return AC_RERENDER | AC_UPDATE_STATE;
+    }
 
     return 0;
 }
--- src/action.h
+++ src/action.h
@@ -8,11 +8,16 @@ static const int AC_RERENDER      = 1 << 0;
 static const int AC_UPDATE_STATE = 1 << 1;
 // clang-format on
 
-// enum SectionType { SC_UNTRACKED, SC_UNSTAGED, SC_STAGED };
-
 int section_action(void *section, int ch);
-int file_action(void *file, int ch);
-int hunk_action(void *hunk, int ch);
-int line_action(void *line, int ch);
+
+int untracked_file_action(void *file_path, int ch);
+int unstaged_file_action(void *file, int ch);
+int staged_file_action(void *file, int ch);
+
+int unstaged_hunk_action(void *hunk, int ch);
+int staged_hunk_action(void *hunk, int ch);
+
+int unstaged_line_action(void *line, int ch);
+int staged_line_action(void *line, int ch);
 
 #endif  // ACTION_H
