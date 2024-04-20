8f8dc80844b2f4c067cdbcc321997f29bf86ce3e
spevnev <spevnev16@gmail.com>
2024-04-20 20:27:36 +0300

    Improve error handling

--- src/git/exec.c
+++ src/git/exec.c
@@ -26,35 +26,16 @@ int gexec(char *const *args) {
     return exit_code;
 }
 
-char *gexecr(int *output_exit_code, char *const *args) {
-    ASSERT(args != NULL);
-
-    int pipe_fds[2];
-    if (pipe(pipe_fds) == -1) ERROR("Couldn't open pipe.\n");
-    int read_fd = pipe_fds[0];
-    int write_fd = pipe_fds[1];
-
-    pid_t pid = fork();
-    if (pid == -1) ERROR("Couldn't fork process.\n");
-    if (pid == 0) {
-        if (close(read_fd) == -1) exit(1);
-
-        if (dup2(write_fd, STDOUT_FILENO) == -1) exit(1);
-        if (dup2(write_fd, STDERR_FILENO) == -1) exit(1);
-
-        if (execvp("git", args) == -1) exit(errno == ENOENT ? NO_GIT_BINARY : 1);
-    }
-
-    if (close(write_fd) == -1) ERROR("Couldn't close pipe.\n");
-
+char *pipe_read(int fd) {
     size_t buffer_capacity = INITIAL_BUFFER_SIZE;
     size_t buffer_size = buffer_capacity;
     size_t buffer_offset = 0;
+
     char *buffer = (char *) malloc(buffer_capacity + 1);
     if (buffer == NULL) ERROR("Process is out of memory.\n");
 
     int bytes;
-    while ((bytes = read(read_fd, buffer + buffer_offset, buffer_size)) > 0 || (bytes == -1 && errno == EINTR)) {
+    while ((bytes = read(fd, buffer + buffer_offset, buffer_size)) > 0 || (bytes == -1 && errno == EINTR)) {
         if (bytes == -1) continue;
 
         buffer_size -= bytes;
@@ -67,10 +48,44 @@ char *gexecr(int *output_exit_code, char *const *args) {
             if (buffer == NULL) ERROR("Process is out of memory.\n");
         }
     }
-    if (bytes == -1) ERROR("Couldn't read from child's pipe.\n");
-    buffer[buffer_offset] = '\0';
+    if (bytes == -1) {
+        free(buffer);
+        return NULL;
+    }
 
-    if (close(read_fd) == -1) ERROR("Couldn't close pipe.\n");
+    buffer[buffer_offset] = '\0';
+    return buffer;
+}
+
+char *gexecr(int *output_exit_code, char *const *args) {
+    ASSERT(args != NULL);
+
+    int pipe_fds[2];
+    int error_pipe_fds[2];
+    if (pipe(pipe_fds) == -1) ERROR("Couldn't open pipe.\n");
+    if (pipe(error_pipe_fds) == -1) ERROR("Couldn't open pipe.\n");
+    int read_fd = pipe_fds[0];
+    int write_fd = pipe_fds[1];
+    int error_read_fd = error_pipe_fds[0];
+    int error_write_fd = error_pipe_fds[1];
+
+    pid_t pid = fork();
+    if (pid == -1) ERROR("Couldn't fork process.\n");
+    if (pid == 0) {
+        if (close(read_fd) == -1) exit(1);
+        if (close(error_read_fd) == -1) exit(1);
+
+        if (dup2(write_fd, STDOUT_FILENO) == -1) exit(1);
+        if (dup2(error_write_fd, STDERR_FILENO) == -1) exit(1);
+
+        if (execvp("git", args) == -1) exit(errno == ENOENT ? NO_GIT_BINARY : 1);
+    }
+
+    if (close(write_fd) == -1) ERROR("Couldn't close pipe.\n");
+    if (close(error_write_fd) == -1) ERROR("Couldn't close pipe.\n");
+
+    char *buffer = pipe_read(read_fd);
+    if (buffer == NULL) ERROR("Couldn't read from child's pipe.\n");
 
     int _exit_code;
     int *exit_code = output_exit_code;
@@ -78,13 +93,17 @@ char *gexecr(int *output_exit_code, char *const *args) {
 
     if (waitpid(pid, exit_code, 0) == -1) ERROR("Couldn't wait for child process.\n");
     if (WEXITSTATUS(*exit_code) == NO_GIT_BINARY) ERROR("Couldn't find git binary. Make sure it is in PATH.\n");
-    if (output_exit_code == NULL && WEXITSTATUS(_exit_code) != 0) ERROR("Childs process exited with non-zero exit code.\n");
+    if (output_exit_code == NULL && WEXITSTATUS(_exit_code) != 0) {
+        char *buffer = pipe_read(error_read_fd);
+        ERROR("Childs process exited with non-zero exit code. Child's stderr:\n%s\n", buffer);
+    }
 
+    if (close(error_read_fd) == -1) ERROR("Couldn't close pipe.\n");
     return buffer;
 }
 
-int gexecw(char *const *args, const char *patch) {
-    ASSERT(args != NULL && patch != NULL);
+int gexecw(char *const *args, const char *buffer) {
+    ASSERT(args != NULL && buffer != NULL);
 
     int pipe_fds[2];
     if (pipe(pipe_fds) == -1) ERROR("Couldn't open pipe.\n");
@@ -105,9 +124,9 @@ int gexecw(char *const *args, const char *patch) {
 
     if (close(read_fd) == -1) ERROR("Couldn't close pipe.\n");
 
-    ssize_t patch_size = strlen(patch);
-    ssize_t bytes = write(write_fd, patch, patch_size);
-    if (bytes != patch_size) ERROR("Couldn't write the entire patch.\n");
+    ssize_t patch_size = strlen(buffer);
+    ssize_t bytes = write(write_fd, buffer, patch_size);
+    if (bytes != patch_size) ERROR("Couldn't write the entire buffer.\n");
     if (close(write_fd) == -1) ERROR("Couldn't close pipe.\n");
 
     int exit_code;
--- src/git/exec.h
+++ src/git/exec.h
@@ -6,12 +6,12 @@
 // Runs git `args` and returns child's exit code.
 int gexec(char *const *args);
 
-// Runs git `args` and returns malloc()-ed merged stdout and stderr to `output`.
+// Runs git `args` and returns malloc()-ed stdout.
 // If `exit_code` is not null, writes child's exit code to it.
 char *gexecr(int *exit_code, char *const *args);
 
-// Runs git `args` and writes `patch` to its standard input.
+// Runs git `args` and writes `buffer` to its standard input.
 // Returns child's exit code.
-int gexecw(char *const *args, const char *patch);
+int gexecw(char *const *args, const char *buffer);
 
 #endif  // EXEC_H
