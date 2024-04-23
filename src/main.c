7a640512e1f893b45ec4b14581b470712003b7b8
spevnev <spevnev16@gmail.com>
2024-04-23 19:00:06 +0300

    Refactor main;Separate help handling

--- src/main.c
+++ src/main.c
@@ -34,9 +34,22 @@
 #define SCREEN_PADDING 2
 
 static bool running = true;
+static bool show_help = false;
 static State state = {0};
 
+#ifdef __linux__
+static int inotify_fd = -1;
+static bool ignore_inotify = false;
+
+static struct pollfd poll_fds[2];
+#else
+static struct pollfd poll_fds[1];
+#endif
+
 static void cleanup(void) {
+#ifdef __linux__
+    close(inotify_fd);
+#endif
     ui_cleanup();
     free_state(&state);
 }
@@ -57,7 +70,7 @@ static void resize(int signal) {
 #ifdef __linux__
 // Recursively adds directory and its subdirectories to inotify.
 // NOTE: modifies path, which must fit longest possible path.
-static void watch_dir(int inotify_fd, char *path) {
+static void watch_dir(char *path) {
     ASSERT(path != NULL);
     if (is_ignored(path)) return;
 
@@ -85,6 +98,97 @@ static void watch_dir(int inotify_fd, char *path) {
 }
 #endif
 
+bool poll_events(void) {
+    int events = poll(poll_fds, sizeof(poll_fds) / sizeof(struct pollfd), -1);
+    if (events == -1) {
+        if (errno == EINTR) return false;
+        ERROR("Unable to poll.\n");
+    }
+
+#ifdef __linux__
+    static char path_buffer[4096];
+    static char event_buffer[1024];
+
+    if (poll_fds[1].revents & POLLIN) {
+        ssize_t bytes;
+        int new_dir = 0;
+        while ((bytes = read(inotify_fd, event_buffer, sizeof(event_buffer))) > 0) {
+            struct inotify_event *event = (struct inotify_event *) event_buffer;
+            if (new_dir) break;
+            for (int i = events; i >= 0; i--) {
+                if ((event->mask & IN_CREATE) && (event->mask & IN_ISDIR)) {
+                    new_dir = 1;
+                    break;
+                }
+                event++;
+            }
+            return false;
+        }
+        if (bytes == -1 && errno != EAGAIN) ERROR("Unable to read inotify event.\n");
+        if (new_dir) {
+            path_buffer[0] = '.';
+            path_buffer[1] = '\0';
+            watch_dir(inotify_fd, path_buffer);
+        }
+
+        if (ignore_inotify) {
+            ignore_inotify = false;
+        } else {
+            // if a key is pressed during external update, ignore that key
+            if (poll_fds[0].revents & POLLIN) {
+                while (getch() != ERR) return false;
+            }
+
+            update_git_state(&state);
+            render(&state);
+            return false;
+        }
+    }
+#endif
+
+    return true;
+}
+
+// TODO: rename
+void handle_info(void) {
+    refresh();
+
+    if (!poll_events()) return;
+
+    int ch;
+    while ((ch = getch()) != ERR) {
+        if (ch == 'q' || ch == KEY_ESCAPE) running = 0;
+    }
+}
+
+void handle_help(void) {
+    static int help_scroll = 0;
+
+    output_help(help_scroll);
+    refresh();
+
+    if (!poll_events()) return;
+
+    int ch;
+    while ((ch = getch()) != ERR) {
+        int mouse = 0;
+        if (ch == KEY_MOUSE) {
+            MEVENT mouse_event;
+            getmouse(&mouse_event);
+            mouse = mouse_event.bstate;
+        }
+
+        if (ch == 'q' || ch == KEY_ESCAPE || ch == 'h') {
+            show_help = false;
+            help_scroll = 0;
+        } else if (ch == 'j' || ch == KEY_DOWN || mouse == MOUSE_SCROLL_DOWN) {
+            if (help_scroll + getmaxy(stdscr) < get_help_length()) help_scroll++;
+        } else if (ch == 'k' || ch == KEY_UP || mouse == MOUSE_SCROLL_UP) {
+            if (help_scroll > 0) help_scroll--;
+        }
+    }
+}
+
 int main(int argc, char **argv) {
     if (argc > 1) {
         fprintf(stderr, "usage: %s\n", argv[0]);
@@ -102,93 +206,46 @@ int main(int argc, char **argv) {
     action.sa_handler = resize;
     sigaction(SIGWINCH, &action, NULL);
 
+    poll_fds[0] = (struct pollfd){STDIN_FILENO, POLLIN, 0};
 #ifdef __linux__
-    char event_buffer[1024];
+    inotify_fd = inotify_init1(IN_NONBLOCK);
+    poll_fds[1] = (struct pollfd){inotify_fd, POLLIN, 0};
+
     char path_buffer[4096] = ".";
-
-    int inotify_fd = inotify_init1(IN_NONBLOCK);
     watch_dir(inotify_fd, path_buffer);
-
-    size_t poll_fds_num = 2;
-    struct pollfd poll_fds[2] = {{STDIN_FILENO, POLLIN, 0}, {inotify_fd, POLLIN, 0}};
-#else
-    size_t poll_fds_num = 1;
-    struct pollfd poll_fds[1] = {{STDIN_FILENO, POLLIN, 0}};
 #endif
 
     ui_init();
     atexit(cleanup);
     render(&state);
 
-    int scroll = 0;
-    int cursor = 0;
-    int selection = -1;
-    bool show_help = false;
-    int saved_scroll = 0;
-    bool ignore_inotify = false;
-    MEVENT mouse_event = {0};
+    MEVENT mouse_event;
+    int scroll = 0, cursor = 0, selection = -1;
     while (running) {
-        if (getmaxx(stdscr) < MIN_WIDTH || getmaxy(stdscr) < MIN_HEIGHT) {
-            clear();
-            printw("Screen is too small! Make sure it is at least %dx%d.\n", MIN_WIDTH, MIN_HEIGHT);
-            refresh();
+        clear();
 
-            // TODO: poll
-            nodelay(stdscr, false);
-            if (getch() == 'q') running = false;
-            nodelay(stdscr, true);
+        if (getmaxx(stdscr) < MIN_WIDTH || getmaxy(stdscr) < MIN_HEIGHT) {
+            printw("Screen is too small! Make sure it is at least %dx%d.\n", MIN_WIDTH, MIN_HEIGHT);
+            handle_info();
             continue;
         }
 
         if (is_state_empty(&state)) {
-            clear();
             printw("There are no unstaged changes.\n");
-            refresh();
-
-            int events = poll(poll_fds, poll_fds_num, -1);
-            if (events == -1) {
-                if (errno == EINTR) continue;
-                ERROR("Unable to poll.\n");
-            }
-
-#ifdef __linux__
-            if (poll_fds[1].revents & POLLIN) {
-                char buffer[1024];
-                ssize_t bytes;
-                int new_dir = 0;
-                while ((bytes = read(inotify_fd, buffer, sizeof(buffer))) > 0) {
-                    struct inotify_event *event = (struct inotify_event *) buffer;
-                    if (new_dir) break;
-                    for (int i = events; i >= 0; i--) {
-                        if ((event->mask & IN_CREATE) && (event->mask & IN_ISDIR)) {
-                            new_dir = 1;
-                            break;
-                        }
-                        event++;
-                    }
-                    continue;
-                }
-                if (bytes == -1 && errno != EAGAIN) ERROR("Unable to read inotify event.\n");
-                if (new_dir) {
-                    path_buffer[0] = '.';
-                    path_buffer[1] = '\0';
-                    watch_dir(inotify_fd, path_buffer);
-                }
-
-                update_git_state(&state);
-                render(&state);
-                continue;
-            }
-#endif
-
-            if (getch() == 'q') running = false;
-
+            handle_info();
             continue;
         }
 
-        int y = scroll + cursor;
-        int height = getmaxy(stdscr);
+        if (show_help) {
+            handle_help();
+            continue;
+        }
 
+        // make sure there is always content on the screen
+        if (scroll > get_lines_length()) scroll = MAX(0, get_lines_length() - cursor);
+
+        int height = getmaxy(stdscr);
+        int y = scroll + cursor;
         int selection_start = -1;
         int selection_end = -1;
         if (selection != -1) {
@@ -196,64 +253,11 @@ int main(int argc, char **argv) {
             selection_end = MAX(selection, y);
         }
 
-        clear();
-        if (show_help) {
-            output_help(scroll);
-        } else {
-            // make sure there is always content on the screen
-            if (scroll > get_lines_length()) scroll = MAX(0, get_lines_length() - cursor);
-
-            height -= output(scroll, cursor, selection_start, selection_end);
-            move(cursor, 0);
-        }
+        height -= output(scroll, cursor, selection_start, selection_end);
+        move(cursor, 0);
         refresh();
 
-        int events = poll(poll_fds, poll_fds_num, -1);
-        if (events == -1) {
-            if (errno == EINTR) continue;
-            ERROR("Unable to poll.\n");
-        }
-
-#ifdef __linux__
-        if (poll_fds[1].revents & POLLIN) {
-            ssize_t bytes;
-            int new_dir = 0;
-            while ((bytes = read(inotify_fd, event_buffer, sizeof(event_buffer))) > 0) {
-                struct inotify_event *event = (struct inotify_event *) event_buffer;
-                if (new_dir) break;
-                for (int i = events; i >= 0; i--) {
-                    if ((event->mask & IN_CREATE) && (event->mask & IN_ISDIR)) {
-                        new_dir = 1;
-                        break;
-                    }
-                    event++;
-                }
-                continue;
-            }
-            if (bytes == -1 && errno != EAGAIN) ERROR("Unable to read inotify event.\n");
-            if (new_dir) {
-                path_buffer[0] = '.';
-                path_buffer[1] = '\0';
-                watch_dir(inotify_fd, path_buffer);
-            }
-
-            if (ignore_inotify) {
-                ignore_inotify = false;
-            } else {
-                // if a key is pressed during external update, ignore that key
-                if (poll_fds[0].revents & POLLIN) {
-                    while (getch() != ERR) continue;
-                }
-
-                update_git_state(&state);
-                render(&state);
-                continue;
-            }
-        }
-
-#else
-        (void) ignore_inotify;
-#endif
+        if (!poll_events()) continue;
 
         int ch;
         while ((ch = getch()) != ERR) {
@@ -263,55 +267,38 @@ int main(int argc, char **argv) {
                 mouse = mouse_event.bstate;
             }
 
-            if (show_help) {
-                if (ch == 'q' || ch == KEY_ESCAPE || ch == 'h') {
-                    show_help = false;
-                    scroll = saved_scroll;
-                } else if (ch == 'j' || ch == KEY_DOWN || mouse == MOUSE_SCROLL_DOWN) {
-                    if (scroll + height < get_help_length()) scroll++;
-                } else if (ch == 'k' || ch == KEY_UP || mouse == MOUSE_SCROLL_UP) {
-                    if (scroll > 0) scroll--;
-                }
-            } else {
-                if (ch == 'q' || ch == KEY_ESCAPE) {
-                    running = false;
-                } else if (ch == 'h') {
-                    show_help = true;
-                    saved_scroll = scroll;
-                    scroll = 0;
-                } else if (ch == 'j' || ch == KEY_DOWN || mouse == MOUSE_SCROLL_DOWN) {
-                    if (cursor < height - 1 - SCREEN_PADDING) cursor++;
-                    else if (scroll + height < get_lines_length()) {
-                        if (cursor > height - 1 - SCREEN_PADDING) cursor--;
-                        scroll++;
-                    } else if (cursor < height - 1) cursor++;
+            if (ch == 'q' || ch == KEY_ESCAPE) {
+                running = false;
+            } else if (ch == 'h') {
+                show_help = true;
+            } else if (ch == 'j' || ch == KEY_DOWN || mouse == MOUSE_SCROLL_DOWN) {
+                if (cursor < height - 1 - SCREEN_PADDING) cursor++;
+                else if (scroll + height < get_lines_length()) {
+                    if (cursor > height - 1 - SCREEN_PADDING) cursor--;
+                    scroll++;
+                } else if (cursor < height - 1) cursor++;
 
-                    if (!is_selectable(scroll + cursor)) selection = -1;
-                } else if (ch == 'k' || ch == KEY_UP || mouse == MOUSE_SCROLL_UP) {
-                    if (cursor > SCREEN_PADDING) cursor--;
-                    else if (scroll > 0) scroll--;
-                    else if (cursor > 0) cursor--;
+                if (!is_selectable(scroll + cursor)) selection = -1;
+            } else if (ch == 'k' || ch == KEY_UP || mouse == MOUSE_SCROLL_UP) {
+                if (cursor > SCREEN_PADDING) cursor--;
+                else if (scroll > 0) scroll--;
+                else if (cursor > 0) cursor--;
 
-                    if (!is_selectable(scroll + cursor)) selection = -1;
-                } else if (y < get_lines_length()) {
-                    int result = invoke_action(y, ch, selection_start, selection_end);
-                    if (result & AC_UPDATE_STATE) {
-                        ignore_inotify = true;
-                        update_git_state(&state);
-                    }
-                    if ((result & AC_RERENDER) || (result & AC_UPDATE_STATE)) render(&state);
-                    if (result & AC_TOGGLE_SELECTION) {
-                        if (selection == -1) selection = y;
-                        else selection = -1;
-                    }
+                if (!is_selectable(scroll + cursor)) selection = -1;
+            } else if (y < get_lines_length()) {
+                int result = invoke_action(y, ch, selection_start, selection_end);
+                if (result & AC_UPDATE_STATE) {
+#ifdef __linux__
+                    ignore_inotify = true;
+#endif
+                    update_git_state(&state);
+                    render(&state);
                 }
+                if (result & AC_RERENDER) render(&state);
+                if (result & AC_TOGGLE_SELECTION) selection = selection == -1 ? y : -1;
             }
         }
     }
-
-#ifdef __linux__
-    close(inotify_fd);
-#endif
 
     return EXIT_SUCCESS;
 }
