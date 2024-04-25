850536b12793cc5e68e357d443381c862d10e3cc
spevnev <spevnev16@gmail.com>
2024-04-25 20:30:58 +0300

    Move keybindings out of main()

--- src/main.c
+++ src/main.c
@@ -26,8 +26,7 @@ static void cleanup(void) {
     free_state(&state);
 }
 
-
-void handle_info(void) {
+static void handle_info(void) {
     refresh();
 
     if (!poll_events(&state)) return;
@@ -38,7 +37,7 @@ void handle_info(void) {
     }
 }
 
-void handle_help(void) {
+static void handle_help(void) {
     static int help_scroll = 0;
 
     output_help(help_scroll);
@@ -75,6 +74,97 @@ void handle_help(void) {
     }
 }
 
+static void handle_main(void) {
+    static int scroll = 0, cursor = 0, selection = -1;
+
+    // make sure there is always content on the screen
+    if (scroll > get_lines_length()) scroll = MAX(0, get_lines_length() - cursor);
+
+    int height = getmaxy(stdscr);
+    int y = scroll + cursor;
+    int selection_start = -1;
+    int selection_end = -1;
+    if (selection != -1) {
+        selection_start = MIN(selection, y);
+        selection_end = MAX(selection, y);
+    }
+
+    height -= output(scroll, cursor, selection_start, selection_end);
+    move(cursor, 0);
+    refresh();
+
+    if (!poll_events(&state)) return;
+
+    int ch;
+    while ((ch = getch()) != ERR) {
+        if (ch == KEY_MOUSE) {
+            MEVENT mouse_event;
+            getmouse(&mouse_event);
+            ch = mouse_event.bstate;
+        }
+
+        switch (ch) {
+            case KEY_ESCAPE:
+            case 'q':
+                running = false;
+                break;
+            case 'h':
+                show_help = true;
+                break;
+            case MOUSE_SCROLL_DOWN:
+            case KEY_DOWN:
+            case 'j':
+                if (cursor < height - 1 - SCROLL_PADDING) cursor++;
+                else if (scroll + height < get_lines_length()) {
+                    if (cursor > height - 1 - SCROLL_PADDING) cursor--;
+                    scroll++;
+                } else if (cursor < height - 1) cursor++;
+
+                if (!is_selectable(scroll + cursor)) selection = -1;
+
+                break;
+            case 'J': {
+                int hunk_y = get_next_hunk(y);
+                if (hunk_y == -1) break;
+
+                if (hunk_y < scroll + getmaxy(stdscr) - SCROLL_PADDING) cursor = hunk_y - scroll;
+                else scroll = hunk_y - cursor;
+            } break;
+            case MOUSE_SCROLL_UP:
+            case KEY_UP:
+            case 'k':
+                if (cursor > SCROLL_PADDING) cursor--;
+                else if (scroll > 0) scroll--;
+                else if (cursor > 0) cursor--;
+
+                if (!is_selectable(scroll + cursor)) selection = -1;
+
+                break;
+            case 'K': {
+                int hunk_y = get_prev_hunk(y);
+                if (hunk_y == -1) break;
+
+                if (hunk_y <= SCROLL_PADDING) {
+                    scroll = 0;
+                    cursor = hunk_y;
+                } else if (hunk_y > scroll + SCROLL_PADDING) cursor = hunk_y - scroll;
+                else scroll = hunk_y - cursor;
+            } break;
+            default:
+                if (y < get_lines_length()) {
+                    int result = invoke_action(y, ch, selection_start, selection_end);
+                    if (result & AC_UPDATE_STATE) {
+                        poll_ignore_change();
+                        update_git_state(&state);
+                        render(&state);
+                    }
+                    if (result & AC_RERENDER) render(&state);
+                    if (result & AC_TOGGLE_SELECTION) selection = selection == -1 ? y : -1;
+                }
+        }
+    }
+}
+
 int main(int argc, char **argv) {
     if (argc > 1) {
         fprintf(stderr, "usage: %s\n", argv[0]);
@@ -90,7 +180,6 @@ int main(int argc, char **argv) {
     atexit(cleanup);
     render(&state);
 
-    int scroll = 0, cursor = 0, selection = -1;
     while (running) {
         clear();
 
@@ -111,92 +200,7 @@ int main(int argc, char **argv) {
             continue;
         }
 
-        // make sure there is always content on the screen
-        if (scroll > get_lines_length()) scroll = MAX(0, get_lines_length() - cursor);
-
-        int height = getmaxy(stdscr);
-        int y = scroll + cursor;
-        int selection_start = -1;
-        int selection_end = -1;
-        if (selection != -1) {
-            selection_start = MIN(selection, y);
-            selection_end = MAX(selection, y);
-        }
-
-        height -= output(scroll, cursor, selection_start, selection_end);
-        move(cursor, 0);
-        refresh();
-
-        if (!poll_events(&state)) continue;
-
-        int ch;
-        while ((ch = getch()) != ERR) {
-            if (ch == KEY_MOUSE) {
-                MEVENT mouse_event;
-                getmouse(&mouse_event);
-                ch = mouse_event.bstate;
-            }
-
-            switch (ch) {
-                case KEY_ESCAPE:
-                case 'q':
-                    running = false;
-                    break;
-                case 'h':
-                    show_help = true;
-                    break;
-                case MOUSE_SCROLL_DOWN:
-                case KEY_DOWN:
-                case 'j':
-                    if (cursor < height - 1 - SCROLL_PADDING) cursor++;
-                    else if (scroll + height < get_lines_length()) {
-                        if (cursor > height - 1 - SCROLL_PADDING) cursor--;
-                        scroll++;
-                    } else if (cursor < height - 1) cursor++;
-
-                    if (!is_selectable(scroll + cursor)) selection = -1;
-
-                    break;
-                case 'J': {
-                    int hunk_y = get_next_hunk(y);
-                    if (hunk_y == -1) break;
-
-                    if (hunk_y < scroll + getmaxy(stdscr) - SCROLL_PADDING) cursor = hunk_y - scroll;
-                    else scroll = hunk_y - cursor;
-                } break;
-                case MOUSE_SCROLL_UP:
-                case KEY_UP:
-                case 'k':
-                    if (cursor > SCROLL_PADDING) cursor--;
-                    else if (scroll > 0) scroll--;
-                    else if (cursor > 0) cursor--;
-
-                    if (!is_selectable(scroll + cursor)) selection = -1;
-
-                    break;
-                case 'K': {
-                    int hunk_y = get_prev_hunk(y);
-                    if (hunk_y == -1) break;
-
-                    if (hunk_y <= SCROLL_PADDING) {
-                        scroll = 0;
-                        cursor = hunk_y;
-                    } else if (hunk_y > scroll + SCROLL_PADDING) cursor = hunk_y - scroll;
-                    else scroll = hunk_y - cursor;
-                } break;
-                default:
-                    if (y < get_lines_length()) {
-                        int result = invoke_action(y, ch, selection_start, selection_end);
-                        if (result & AC_UPDATE_STATE) {
-                            poll_ignore_change();
-                            update_git_state(&state);
-                            render(&state);
-                        }
-                        if (result & AC_RERENDER) render(&state);
-                        if (result & AC_TOGGLE_SELECTION) selection = selection == -1 ? y : -1;
-                    }
-            }
-        }
+        handle_main();
     }
 
     return EXIT_SUCCESS;
